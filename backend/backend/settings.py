aff75d852485dfd1335e2d1ee18c4632d2332e25
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2025-05-08 09:50:44 -0400

    feat: :sparkles: Se crean modelos menu y productos en api junto a sus endpoints
    Se crean los modelos para registrar productos en el menu junto a su categoría, además se crean sus endpoints

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.users",
+    "apps.menu",
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt.token_blacklist",
